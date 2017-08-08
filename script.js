ec07c5e4f50863f16959746d65f8b63a478f84f3
hoture <myam0118@gmail.com>
2017-08-08 15:23:23 +0900

    support kindle store

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 $(function() {
-  $('div.review').each(function() {
+  // for book etc.
+  convertVoteTexts('div[id^=customer_review]', 'a.review-title', 'span.review-votes', 'tr td div div:first')
+  // for kindle etc.
+  convertVoteTexts('div[id^=rev-]', 'a.a-link-normal.a-text-normal.a-color-base:first', 'span.a-size-base.a-color-secondary.cr-vote-buttons', 'tr td div div:first')
+})
+
+function convertVoteTexts(reviewQuery, linkQuery, defaultVoteTextQuery, helpfulVoteTextQuery) {
+  $(reviewQuery).each(function() {
     var $this = $(this)
 
-    var reviewPageUrl = $this.find('a.review-title').attr('href')
+    var reviewPageUrl = $this.find(linkQuery).attr('href')
+    console.log(reviewPageUrl)
     $.ajax({
       url: reviewPageUrl,
       type: 'GET',
     }).done(function(data) {
-      var helpfulVoteText = $(data).find('tr td div div:first').text()
-      $this.find('span.review-votes').text(helpfulVoteText)
+      var helpfulVoteText = $(data).find(helpfulVoteTextQuery).text()
+      console.log(helpfulVoteText)
+      $this.find(defaultVoteTextQuery).text(helpfulVoteText)
     })
   })
-})
+}
